9563a7c4fea74cdd7224bc16cbc8dcd1b20de85e
saiharshith22 <saiharshithjyothula@gmail.com>
2026-01-17 15:45:40 +0530

    created eqly backend application

---
+++ eqly-backend/src/main/java/com/eqly/eqly_backend/EqlyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eqly.eqly_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EqlyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EqlyBackendApplication.class, args);
+	}
+
+}
